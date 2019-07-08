1e603fc1c7ddc959d7b003fde4aa7da5f666f5a2
Deepak <deepak.sharma7206@gmail.com>
2019-07-08 17:57:02 +0530

    Intial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app=express();
+
+hbs.registerPartials(__dirname + '/views/partial');
+app.set('View Engine', 'hbs')
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+next();
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+app.get('/',(req, res)=>{
+  // res.send('<h1>Hello Express</h1>');
+  // res.send({
+  //   name:'Deepak',
+  //   likes:[
+  //     'biking',
+  //     'cities'
+  //   ]
+  // });
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+
+  });
+});
+
+app.get('/about',(req,res)=>{
+  // res.send("About Page");
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    ErrorMEssage: 'somethinf=g went wrong'
+  });
+});
+app.listen(3000,()=>{
+  console.log('Server is started on port 3000');
+});
